5b9482d4ab8241a22d16fb6da3dfb9b28c5c4b21
Suraj Ghodke <surajghodke0102@gmail.com>
2021-03-02 19:52:06 +0530

    db connection and other required packages

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,21 @@
-const express = require("express");
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+//database connection
+connectDB();
 
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("hello");
+  res.send("API is running");
 });
-app.listen(5000, console.log("server running"));
+
+//connection
+const PORT = process.env.PORT || 5000;
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
