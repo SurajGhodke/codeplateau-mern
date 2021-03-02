8d96311a8201f06e50a54fd524df103b227dc71a
Suraj Ghodke <surajghodke0102@gmail.com>
2021-03-02 23:50:36 +0530

    backend completed

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 
@@ -9,9 +11,18 @@ connectDB();
 
 const app = express();
 
+app.use(express.json());
+
+//routes
+app.use("/api/users", userRoutes);
+
 app.get("/", (req, res) => {
   res.send("API is running");
 });
+
+//custom error middleware
+app.use(notFound);
+app.use(errorHandler);
 
 //connection
 const PORT = process.env.PORT || 5000;
